377750cb89169b753c368a40f039214b84be36c9
AnushaMishra <am284401@gmail.com>
2024-02-25 00:49:54 -0600

    updated test_mpe import

--- tests/mpe/test_mpe.py
+++ tests/mpe/test_mpe.py
@@ -7,6 +7,10 @@ import numpy as np
 from pettingzoo.test import parallel_api_test
 from pettingzoo.mpe import simple_v3, simple_world_comm_v3, simple_tag_v3, simple_spread_v3, simple_crypto_v3, simple_speaker_listener_v4, simple_push_v3, simple_adversary_v3, simple_reference_v3
 import tqdm
+import sys
+sys.path.append('/Users/anushamishra/Documents/Human-AI Research/Anusha/JaxMARL/')  # Add environments directory to sys.path
+# # import environments.overcooked
+# from environments.overcooked.common import OBJECT_TO_INDEX, COLOR_TO_INDEX, COLORS
 from jaxmarl import make
 import pytest
 
